1a64f22fec351e6e45590d7a2ef90b586f2c1f4a
Zixuan Chen <remch183@outlook.com>
2022-05-20 13:12:36 +0800

    feat: add setup script

---
+++ README.md
@@ -0,0 +1,12 @@
+To reproduce your issue on Vitest VS Code extension
+
+1.  `git clone https://github.com/zxch3n/vitest-ext-basic-example.git`
+2.  Run `node setup.mjs`. It will
+    - Install deps
+    - Update Vitest extension to the latest pre-release version
+    - Unify Vitest settings
+    - Log env info to info.txt
+3.  Open/Reload this project in VS Code
+4.  Test Vitest extension on this project. If the issue persists, please
+    [report the issue](https://github.com/vitest-dev/vscode/issues/new?assignees=&labels=bug&template=bug_report.md&title=)
+    with content in info.txt
---
+++ setup.mjs
@@ -0,0 +1,68 @@
+import { exec } from "child_process";
+import { writeFile } from "fs";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { resolve } from "path";
+import { promisify } from "util";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+async function main() {
+  try {
+    await run("yarn");
+  } catch (e) {
+    await run("npm install");
+  }
+
+  await promisify(writeFile)(
+    resolve(__dirname, ".vscode/settings.json"),
+    JSON.stringify(
+      {
+        "vitest.commandLine": "./node_modules/.bin/vitest",
+        "vitest.nodeEnv": { PATH: process.env.PATH },
+      },
+      undefined,
+      2
+    )
+  );
+
+  console.log("Vitest settings updated");
+  const { default: envinfo } = await import("envinfo");
+  const infoStr = await envinfo.run(
+    {
+      System: ["OS"],
+      Binaries: ["Node", "Yarn", "npm"],
+      npmPackages: ["vite", "vitest"],
+      IDEs: ["VSCode"],
+    },
+    { json: true, showNotFound: true }
+  );
+  const info = JSON.parse(infoStr);
+  const code = info["IDEs"]["VSCode"] && info["IDEs"]["VSCode"]["path"];
+  if (code) {
+    await run(
+      `${code} --install-extension zixuanchen.vitest-explorer --pre-release --force`
+    );
+  } else {
+    throw new Error("VS Code not found");
+  }
+
+  await promisify(writeFile)(resolve(__dirname, "info.txt"), infoStr);
+}
+
+function run(command) {
+  const child = exec(command);
+  child.stdout.pipe(process.stdout);
+  child.stderr.pipe(process.stderr);
+  return new Promise((resolve, reject) => {
+    child.on("exit", function (code) {
+      if (code === 0) {
+        resolve();
+      } else {
+        reject();
+      }
+    });
+  });
+}
+
+main();
